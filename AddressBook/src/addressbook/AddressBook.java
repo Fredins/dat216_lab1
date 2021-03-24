6e7e7a77d3b64ca164f37d04b3194ca45c679b5e
Martin Fredin <fredin.martin@gmail.com>
2021-03-24 17:40:50 +0100

    lab finished

--- AddressBook/src/addressbook/AddressBook.java
+++ AddressBook/src/addressbook/AddressBook.java
@@ -6,22 +6,25 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import se.chalmers.cse.dat215.lab1.Model;
 
 public class AddressBook extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-
+        
         ResourceBundle bundle = java.util.ResourceBundle.getBundle("addressbook/resources/AddressBook");
-
-
+        
         Parent root = FXMLLoader.load(getClass().getResource("address_book.fxml"), bundle);
         
         Scene scene = new Scene(root, 600, 400);
         
         stage.setTitle(bundle.getString("application.name"));
         stage.setScene(scene);
+        stage.setMinWidth(300);
+        stage.getIcons().add(new Image("addressbook/resources/frameIcon32.gif"));
         stage.show();
     }
 
@@ -30,6 +33,12 @@ public class AddressBook extends Application {
      */
     public static void main(String[] args) {
         launch(args);
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            public void run() {
+                Model.getInstance().shutDown();
+            }
+        }));
+
     }
     
 }
--- AddressBook/src/addressbook/AddressBookController.java
+++ AddressBook/src/addressbook/AddressBookController.java
@@ -10,17 +10,67 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.MenuBar;
+import javafx.scene.control.*;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import se.chalmers.cse.dat215.lab1.Presenter;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 
 public class AddressBookController implements Initializable {
     
     @FXML private MenuBar menuBar;
-    
+
+    @FXML private Button newButton;
+    @FXML private Button deleteButton;
+
+    @FXML private ListView contactsListView;
+
+    @FXML private TextField firstNameTextField;
+    @FXML private TextField lastNameTextField;
+    @FXML private TextField phoneTextField;
+    @FXML private TextField emailTextField;
+    @FXML private TextField adressTextField;
+    @FXML private TextField postCodeTextField;
+    @FXML private TextField cityTextField;
+
+    @FXML private Menu fileMenu;
+    @FXML private Menu helpMenu;
+
+    @FXML private MenuItem newContact;
+    @FXML private MenuItem deleteContact;
+    @FXML private SeparatorMenuItem separatorMenuItem;
+    @FXML private MenuItem exit;
+
+    @FXML private Presenter presenter;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+        presenter = new Presenter(
+                contactsListView,
+                firstNameTextField,
+                lastNameTextField,
+                phoneTextField,
+                emailTextField,
+                adressTextField,
+                postCodeTextField,
+                cityTextField);
+        presenter.init();
+        contactsListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                presenter.contactsListChanged();
+            }
+
+        });
+        firstNameTextField.focusedProperty().addListener(new TextFieldListener(firstNameTextField));
+        lastNameTextField.focusedProperty().addListener(new TextFieldListener(lastNameTextField));
+        phoneTextField.focusedProperty().addListener(new TextFieldListener(phoneTextField));
+        emailTextField.focusedProperty().addListener(new TextFieldListener(emailTextField));
+        adressTextField.focusedProperty().addListener(new TextFieldListener(adressTextField));
+        postCodeTextField.focusedProperty().addListener(new TextFieldListener(postCodeTextField));
+        cityTextField.focusedProperty().addListener(new TextFieldListener(cityTextField));
     }
     
     @FXML 
@@ -41,5 +91,37 @@ public class AddressBookController implements Initializable {
         
         Stage addressBookStage = (Stage) menuBar.getScene().getWindow();
         addressBookStage.hide();
-    }    
+    }
+
+    @FXML
+    protected void newContactActionPerformed(ActionEvent event) throws IOException{
+        presenter.newContact();
+    }
+
+    @FXML
+    protected void deleteContactActionPerformed(ActionEvent event) throws IOException{
+        presenter.removeCurrentContact();
+    }
+
+    private class TextFieldListener implements ChangeListener<Boolean>{
+
+        private TextField textField;
+
+        public TextFieldListener(TextField textField){
+            this.textField = textField;
+        }
+
+        @Override
+        public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+            if(newValue){
+                presenter.textFieldFocusGained(textField);
+
+            }
+            else{
+                presenter.textFieldFocusLost(textField);
+            }
+        }
+    }
+
+
 }
